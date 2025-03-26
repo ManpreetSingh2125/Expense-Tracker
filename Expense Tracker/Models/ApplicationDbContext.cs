42f56b08a4f9fb29e27a65c3b7520184c7c8f301
Manpreet Singh <c0929714@mylambton.ca>
2025-03-26 01:05:32 -0400

    Add project files.

---
+++ Expense Tracker/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Expense_Tracker.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {}
+
+        public DbSet<Transaction> TBL_TRANSACTIONS { get; set; }
+        public DbSet<Category> TBL_CATEGORIES { get; set; }
+    }
+}
